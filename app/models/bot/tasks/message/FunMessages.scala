2ce373198d6a33fdf4dfc109d38f4fa1093cf1db
bryandmartin1 <bmartin1@macalester.edu>
2015-02-25 18:37:47 -0600

    Update FunMessages.scala

--- app/models/bot/tasks/message/FunMessages.scala
+++ app/models/bot/tasks/message/FunMessages.scala
@@ -21,11 +21,7 @@ object FunMessages {
     ),
     MessageTree(
       value = "I can't wait to introduce you to my mom!",
-      right = Some(MessageTree(
-        value = "Since mom showed everyone my naked baby pictures, I've been looking for ways to get her back.",
-        right = None,
-        left = None
-      )),
+      right = None,
       left = None
     )
   )
